461d13f4277faa56cc41c8672af0765943ca2ee6
GraniteGlasses <vaustun@gmail.com>
2024-07-28 08:51:14 -0500

    Add files via upload
    fixed an issue where pause menu buttons could be pressed when menu was not opened
    added a toggled sprint ability

---
+++ pause_menu.gd
@@ -0,0 +1,51 @@
+extends Control
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+	visible = false
+
+
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+
+func test_escape():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+		visible = true
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
+		visible = false
+
+func _on_messages_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_gallery_camera_pressed():
+	pass # Replace with function body.
+
+
+func _on_quests_pressed():
+	pass # Replace with function body.
+
+
+func _on_save_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _process(delta):
+	test_escape()
